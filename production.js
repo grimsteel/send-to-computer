a903927a8e50fbf199878533016f2ff9c1af0367
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-06-21 00:24:31 -0500

    Update for systemd

--- production.js
+++ production.js
@@ -1,12 +1,9 @@
+#!/usr/bin/env node
+
 import { createServer } from "http";
-import { unlinkSync } from "fs";
 import registerUpgrade from "./websocket.js";
 
 const server = createServer();
 registerUpgrade(server);
 
-try {
-  unlinkSync("/var/run/stc_socket");
-} catch {}
-
-server.listen("/var/run/stc_socket", () => console.log("Listening on /var/run/stc_socket"));
+server.listen("/run/stc/stc_socket", () => console.log("Listening on /run/stc/stc_socket"));
