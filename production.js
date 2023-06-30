b80110219085e75256df44cf5d44dd541f18f134
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-06-30 16:14:46 -0500

    Fix production script

--- production.js
+++ production.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
 import { createServer } from "http";
+import { chmodSync } from "fs";
 import registerUpgrade from "./websocket.js";
+import process from "process";
+
+// Production script. This is automatically run with the correct user (lemon) and group (www-data) by systemctl (stc.service)
+// This only handles the websocket. Static files are served by nginx. This communicates with nginx via the stc_socket. 
 
 const server = createServer();
 registerUpgrade(server);
 
-server.listen("/run/stc/stc_socket", () => console.log("Listening on /run/stc/stc_socket"));
+const socketPath = `${process.env.RUNTIME_DIRECTORY}/stc_socket`;
+
+server.listen(socketPath, () => {
+  chmodSync(socketPath, 0o770);
+  console.log(`Listening on ${socketPath}`);
+});
