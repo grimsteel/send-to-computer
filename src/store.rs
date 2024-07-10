c4054e5473d7a9e3756809fc93a48874be3cb157
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-10 13:50:55 -0500

    fix store bugs, add tests

--- src/store.rs
+++ src/store.rs
@@ -1,16 +1,24 @@
-use std::path::Path;
+use std::{collections::HashMap, fmt::Display, path::Path};
 
-use redb::{AccessGuard, Database, ReadableTable, StorageError, TableDefinition};
+use redb::{backends::InMemoryBackend, AccessGuard, Database, ReadableTable, TableDefinition};
 
 type USERS<'a> = TableDefinition<'a, u16, &'a str>;
 const USERS_TABLE: USERS = TableDefinition::new("users");
 
+#[derive(Debug)]
 pub enum StoreError {
     RedbError(redb::Error),
-    NotFound
 }
 
 impl<T> From<T> for StoreError where T: Into<redb::Error> { fn from(value: T) -> Self { Self::RedbError(value.into()) } }
+impl Display for StoreError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            StoreError::RedbError(err) => write!(f, "Redb Error: {err}")
+        }
+    }
+}
+impl std::error::Error for StoreError {}
 
 type Result<T> = std::result::Result<T, StoreError>;
 
@@ -19,27 +27,64 @@ pub struct Store {
 }
 
 impl Store {
-    fn init<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let db = Database::create(path)?;
+    /// initialize the store on the given path, or in memory if `path` is None
+    pub fn init<P: AsRef<Path>>(path: Option<P>) -> Result<Self> {
+        let db = if let Some(path) = path { Database::create(path)? } else { Database::builder().create_with_backend(InMemoryBackend::new())? };
         Ok(Self {
             db
         })
     }
 
-    pub fn get_username(&self, id: u16) -> Result<AccessGuard<&str>> {
+    pub fn get_username(&self, id: u16) -> Result<Option<AccessGuard<&'static str>>> {
         let tx = self.db.begin_read()?;
-        let users = tx.open_table(USERS_TABLE)?;
-        users.get(id)?.ok_or(StoreError::NotFound)
+        match tx.open_table(USERS_TABLE) {
+            Ok(users) => Ok(users.get(id)?),
+            // if the table doesn't exist just return None
+            Err(redb::TableError::TableDoesNotExist(_)) => Ok(None),
+            Err(e) => Err(e.into())
+        }
     }
 
     pub fn add_user(&self, username: &str) -> Result<()> {
         let tx = self.db.begin_write()?;
-        let mut users = tx.open_table(USERS_TABLE)?;
-        // add one to last key
-        let user_id = users
-            .last()?.map(|v| v.0.value() + 1)
-            .unwrap_or_default();
-        users.insert(user_id, username)?;
+        {
+            let mut users = tx.open_table(USERS_TABLE)?;
+            // add one to last key
+            let user_id = users
+                .last()?.map(|v| v.0.value() + 1)
+                .unwrap_or_default();
+            users.insert(user_id, username)?;
+        }
+        tx.commit()?;
+        Ok(())
+    }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::Store;
+
+    type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+    #[test]
+    fn add_users() -> Result {
+        let store = Store::init::<PathBuf>(None)?;
+
+        assert!(store.get_username(0)?.is_none());
+
+        // add a user
+        store.add_user("foobar")?;
+
+        // make sure they exist
+        assert_eq!(store.get_username(0)?.as_ref().map(|v| v.value()), Some("foobar"));
+
+        // add another user
+        store.add_user("foo")?;
+
+        // make sure they exist
+        assert_eq!(store.get_username(1)?.as_ref().map(|v| v.value()), Some("foo"));
+
         Ok(())
     }
 }
