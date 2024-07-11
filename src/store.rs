1a8f23d25411619f3af0a3ff69255d983e69d600
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-11 15:37:27 -0500

    finish groups

--- src/store.rs
+++ src/store.rs
@@ -77,30 +77,68 @@ impl Store {
         }
     }
 
-    pub fn create_group(&self, name: &str, initial_users: Vec<u16>) -> Result<()> {
+    pub fn create_update_group(&self, name: &str, mut users: Vec<u16>, id: Option<u16>) -> Result<()> {
         let tx = self.db.begin_write()?;
         {
-            let users = tx.open_table(USERS_TABLE)?;
+            let users_table = tx.open_table(USERS_TABLE)?;
             // make sure all of the users exist
-            if !initial_users.iter().all(|id| users.get(id).is_ok_and(|v| v.is_some())) {
+            if !users.iter().all(|id| users_table.get(id).is_ok_and(|v| v.is_some())) {
                 return Err(StoreError::InvalidUserIds);
             }
+
+            // sort the users so we can do binary search
+            users.sort_unstable();
             
             let mut groups = tx.open_table(GROUPS_TABLE)?;
-            // add one to last key
-            let group_id = groups
-                .last()?.map(|v| v.0.value() + 1)
-                .unwrap_or_default();
-            groups.insert(group_id, (name, initial_users))?;
+            let group_id = if let Some(id) = id { id } else {
+                // add one to last key
+                groups.last()?
+                    .map(|v| v.0.value() + 1)
+                    .unwrap_or_default()
+            };
+            groups.insert(group_id, (name, users))?;
         }
         tx.commit()?;
         Ok(())
+    }
+
+    pub fn delete_group(&self, id: u16) -> Result<()> {
+        let tx = self.db.begin_write()?;
+        {            
+            let mut groups = tx.open_table(GROUPS_TABLE)?;
+            groups.remove(id)?;
+            // TODO: delete messages for group
+        }
+        tx.commit()?;
+        Ok(())
+    }
+
+    pub fn get_groups_for_user(&self, user_id: u16) -> Result<HashMap<u16, (String, Vec<u16>)>> {
+        let tx = self.db.begin_read()?;
+
+        // make sure the user exists
+        let users = tx.open_table(USERS_TABLE)?;
+        if !users.get(user_id).is_ok() {
+            return Err(StoreError::InvalidUserIds);
+        }
+
+        let groups = tx.open_table(GROUPS_TABLE)?;
+
+        Ok(groups.iter()?.filter_map(|v| {
+            let v = v.ok()?;
+            let group = v.1.value();
+            // make sure they're a part of this group
+            group.1.binary_search(&user_id).ok()?;
+            Some((v.0.value(), (group.0.into(), group.1)))
+        }).collect())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::{collections::HashMap, path::PathBuf};
+
+    use crate::store::StoreError;
 
     use super::Store;
 
@@ -126,6 +164,35 @@ mod tests {
 
         assert_eq!(store.list_users()?, HashMap::from([(0, "foobar".into()), (1, "foo".into())]));
 
+        Ok(())
+    }
+
+    #[test]
+    fn add_groups() -> Result {
+        let store = Store::init::<PathBuf>(None)?;
+
+        store.create_user("foobar")?;
+
+        // try creating the group with invalid users
+        assert!(matches!(store.create_update_group("foo", vec![0, 1], None), Err(StoreError::InvalidUserIds)));
+
+        store.create_user("foo")?;
+
+        store.create_update_group("foo", vec![0, 1], None)?;
+        store.create_update_group("foobar", vec![1], None)?;
+
+        let group_0 = (0, ("foo".into(), vec![0, 1]));
+        let group_1 = (1, ("foobar".into(), vec![1]));
+
+        // make sure we can read the groups for each user
+        assert_eq!(store.get_groups_for_user(0)?, HashMap::from([group_0.clone()]));
+        assert_eq!(store.get_groups_for_user(1)?, HashMap::from([group_0, group_1]));
+
+        // delete a group
+        store.delete_group(0)?;
+
+        assert_eq!(store.get_groups_for_user(0)?, HashMap::new());
+        
         Ok(())
     }
 }
