3fdd5b749825464391c08b0a583bd93c1a719d74
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-26 15:24:50 -0500

    start on message editing - textarea

--- frontend/components/message-row.ts
+++ frontend/components/message-row.ts
@@ -1,6 +1,7 @@
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 import { StyledElement } from "../css";
-import { html } from "lit";
+import { html, nothing } from "lit";
+import { live } from "lit/directives/live.js";
 
 @customElement("message-row")
 export default class extends StyledElement {
@@ -13,16 +14,38 @@ export default class extends StyledElement {
   @property()
   sender: string;
   @property()
-  tags: string[]
+  tags: string[];
+
+  @state()
+  editing: boolean = false;
 
   render() {
+    const editButton = this.canEdit ? (this.editing ?
+                                       html`` :
+                                       html`
+                                         <button
+                                           class="p-0 text-amber-500 hover:text-amber-600 cursor-pointer me-2"
+                                           type="button" @click=${() => this.editing = true}>
+                                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                                             <path d="M13.488 2.513a1.75 1.75 0 0 0-2.475 0L6.75 6.774a2.75 2.75 0 0 0-.596.892l-.848 2.047a.75.75 0 0 0 .98.98l2.047-.848a2.75 2.75 0 0 0 .892-.596l4.261-4.262a1.75 1.75 0 0 0 0-2.474Z" />
+                                             <path d="M4.75 3.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h6.5c.69 0 1.25-.56 1.25-1.25V9A.75.75 0 0 1 14 9v2.25A2.75 2.75 0 0 1 11.25 14h-6.5A2.75 2.75 0 0 1 2 11.25v-6.5A2.75 2.75 0 0 1 4.75 2H7a.75.75 0 0 1 0 1.5H4.75Z" />
+                                           </svg>
+                                         </button>
+                                       `) : nothing;
+
     return html`
       <p style="overflow-wrap: break-word;" class="flex items-center">
         <strong class="font-semibold me-1">${this.sender}:</strong>
-        <span style="overflow-wrap: break-word" class="min-w-0">${this.message}</span>
+        <!-- editable message -->
+        <textarea
+          style="overflow-wrap: break-word"
+          class="bg-transparent border-0 p-0 focus:text-orange-200 transition-colors focus:outline-0 focus:border-0 focus:ring-0 focus:underline leading-none cursor-text min-w-0 grow me-auto"
+          ?disabled=${!this.editing} .value=${live(this.message)} wrap="soft"></textarea>
+
+        ${editButton}
 
         <!-- copy -->
-        <button class="p-0 text-emerald-500 hover:text-emerald-600 cursor-pointer ms-auto" type="button" @click=${() => navigator.clipboard.writeText(this.message)}>
+        <button class="p-0 text-emerald-500 hover:text-emerald-600 cursor-pointer me-2" type="button" @click=${() => navigator.clipboard.writeText(this.message)}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
             <path fill-rule="evenodd" d="M11.986 3H12a2 2 0 0 1 2 2v6a2 2 0 0 1-1.5 1.937V7A2.5 2.5 0 0 0 10 4.5H4.063A2 2 0 0 1 6 3h.014A2.25 2.25 0 0 1 8.25 1h1.5a2.25 2.25 0 0 1 2.236 2ZM10.5 4v-.75a.75.75 0 0 0-.75-.75h-1.5a.75.75 0 0 0-.75.75V4h3Z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M3 6a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H3Zm1.75 2.5a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5h-3.5ZM4 11.75a.75.75 0 0 1 .75-.75h3.5a.75.75 0 0 1 0 1.5h-3.5a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
@@ -31,7 +54,7 @@ export default class extends StyledElement {
 
         <!-- delete -->
         <button
-          class="p-0 text-rose-500 hover:text-rose-600 cursor-pointer ms-2"
+          class="p-0 text-rose-500 hover:text-rose-600 cursor-pointer me-2"
           type="button" @click=${() => this.dispatchEvent(new Event("delete"))}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
             <path fill-rule="evenodd" d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z" clip-rule="evenodd" />
@@ -46,5 +69,5 @@ export default class extends StyledElement {
           ></tag-list>
       </p>
     `;
-  }
+    }
 }
