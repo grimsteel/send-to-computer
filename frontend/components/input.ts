0ff5e8de1f6692b377f08245348c47c33a7b8dca
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-26 17:15:06 -0500

    fix enter on input

--- frontend/components/input.ts
+++ frontend/components/input.ts
@@ -19,15 +19,23 @@ export default class FormInput extends StyledElement {
     this.inputId = `_${crypto.randomUUID()}`;
   }
 
-  changeHandler(e: InputEvent) {
+  private onChange(e: InputEvent) {
     const value = (e.target as HTMLInputElement).value;
     this.value = value;
   }
 
-  onSubmit(e: SubmitEvent) {
-    e.preventDefault();
+  private onSubmit(e?: SubmitEvent) {
+    e?.preventDefault();
     this.dispatchEvent(new CustomEvent("submit", { composed: true, detail: { value: this.value } }));
     this.value = "";
+  }
+
+  private onKeydown(e: KeyboardEvent) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      this.value = (e.target as HTMLInputElement).value;
+      this.onSubmit();
+    }
   }
   
   render() {
@@ -40,7 +48,7 @@ export default class FormInput extends StyledElement {
           <textarea
                 id=${this.inputId} .value=${this.value}
                 class="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white focus:ring-orange-500 focus:border-orange-500" rows="1"
-                required="required" @change=${this.changeHandler}
+                required="required" @change=${this.onChange} @keydown=${this.onKeydown}
           ></textarea>
         </div>
         <button type="submit" class="text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-orange-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
